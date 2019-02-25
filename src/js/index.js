2556ae1b9e66a2081b26ef9696d5e072f8d18e84
artie c <icewizardwoe@mail.com>
2019-02-24 21:56:39 -0500

    editing css

--- src/js/index.js
+++ src/js/index.js
@@ -6,26 +6,13 @@ import '../styles/style.scss';
 import 'turn.js';
 import $ from 'jquery';
 
-read();
-// //read
-// fetch('http://menu.com/menu/api/post/read.php')
-// .then(res => {
-//     if(res.ok){
-//         return res.json();
-//     } else {
-//         throw Error(`Request rejected with status ${res.status}`);
-//     }
-// })
-// .then((data) => {
-//     console.log(data);
-// })
-// .catch(console.error);
+
 
 
 
 $("#flipbook").turn({
-    width: 400,
-    height: 300,
+    width: 1200,
+    height:900,
     autoCenter: true
   });
   
